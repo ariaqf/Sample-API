2709d9d9ba8dfbbd20bee224690449d5bc584144
Gabriel Fonseca <gabrielsqsf@gmail.com>
2020-03-02 23:22:28 -0300

    name change of parameter for project cohesion

--- logic/search_planet.py
+++ logic/search_planet.py
@@ -1,9 +1,9 @@
 from logic.output import PlanetPageResponse, PlanetResponse
 from logic.exceptions import ParameterException
 
-def search_planet(name, queryable, results_per_page = 10):
+def search_planet(name, queryable, page = 1, records_per_page = 10):
     if(queryable is None):
         raise ParameterException("DataSource is not acceptable")
-    ret = queryable.get_by_name(name, records_per_page = results_per_page)
+    ret = queryable.get_by_name(name, records_per_page = records_per_page, page = page)
     return ret #list(map(lambda x: PlanetResponse(x), ret))
     
--- test/logic/search_by_name_test.py
+++ test/logic/search_by_name_test.py
@@ -33,11 +33,11 @@ def test_search_pages():
     created2 = create_planet('ABC', 'B', 'C', queryable).planet
     created3 = create_planet('BACA', 'B', 'C', queryable).planet
     created4 = create_planet('PPP', 'B', 'C', queryable).planet
-    ret = search_planet(created1.name, queryable, results_per_page = 2)
+    ret = search_planet(created1.name, queryable, records_per_page = 2)
     assert ret.page == 1 
     assert ret.total_pages == 2
     assert len(ret.planets) == 2
-    assert ret.planets[0].name.count('A') > 0
+    assert ret.planets[0].name.count('A') > 0 
     assert ret.planets[1].name.count('A') > 0
     queryable.clean()
 
